b30ce5ef7c1426a7428702e211f737b06ae91b2e
o773h <o773h@naver.com>
2023-02-23 23:04:05 +0900

    230223

---
+++ 2623_음악프로그램.py
@@ -0,0 +1,38 @@
+import sys
+import collections
+
+def topological_sort(graph,indegree):
+    result = []
+    queue = collections.deque()
+
+    for i in range(1,n+1):
+        if indegree[i]==0:
+            queue.append(i)
+    
+    while queue:
+        node = queue.popleft()
+        result.append(node)
+        
+        for next in graph[node]:
+            indegree[next]-=1
+            if indegree[next]==0:
+                queue.append(next)
+                
+    for i in indegree:
+        if i>0:
+            return [0]
+        
+    return result
+
+
+n,m = map(int,sys.stdin.readline().split())
+indegree=[0 for _ in range(n+1)]
+graph=[[] for _ in range(n+1)]
+
+for _ in range(m):
+    lst = list(map(int,sys.stdin.readline().split()))
+    for i in range(2,len(lst)):
+        indegree[lst[i]]+=1
+        graph[lst[i-1]].append(lst[i])
+
+print(*topological_sort(graph,indegree),sep='\n')
