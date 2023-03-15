cac6516b3398d90154870af020cdfc7ca3ea1782
jiwan <o773h@naver.com>
2023-03-15 23:09:11 +0900

    230315

---
+++ 1939_중량제한.py
@@ -0,0 +1,35 @@
+import sys
+sys.setrecursionlimit(int(1e6))
+
+def find_parent(parent,x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent,parent[x])
+    return parent[x]
+
+def union_parent(parent,a,b):
+    a = find_parent(parent,a)
+    b = find_parent(parent,b)
+
+    if a<b:
+        parent[b] = a
+    else:
+        parent[a] = b
+        
+
+n,m = map(int,sys.stdin.readline().split())
+
+parent = [i for i in range(n+1)]
+edges = []
+for _ in range(m):
+    a,b,c = map(int,sys.stdin.readline().split())
+    edges.append((c,a,b))
+
+start,end = map(int,sys.stdin.readline().split())
+edges.sort(reverse=True)
+
+for c,a,b in edges:
+    if find_parent(parent,a) != find_parent(parent,b):
+        union_parent(parent,a,b)
+        if find_parent(parent,start) == find_parent(parent,end):
+            print(c)
+            break
