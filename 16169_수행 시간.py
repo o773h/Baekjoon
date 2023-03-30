19e8366a9713fbbb4191ec6ebe523d5e5dcb5293
jiwan <o773h@naver.com>
2023-03-30 22:59:25 +0900

    230330

---
+++ 16169_수행 시간.py
@@ -0,0 +1,27 @@
+import sys
+
+def solve():
+
+    for i in ranks[1]:
+        total_time[i] = times[i]
+    
+    for i in range(1,n):
+        for node in ranks[i]:
+            for next in ranks[i+1]:
+                total_time[next] = max(total_time[next],(next-node)**2 + total_time[node] + times[next])
+
+
+n = int(sys.stdin.readline())
+
+total_time = [0 for _ in range(n+1)]
+times = [0]
+ranks = [[] for _ in range(n+1)]
+
+for i in range(1,n+1):
+    rank,t = map(int,sys.stdin.readline().split())
+    ranks[rank].append(i)
+    times.append(t)
+
+solve()
+
+print(max(total_time))
